20a5956ef16a13bce01c567dc7ca15358748f99b
Aleff13 <aleff.menna.resler@gmail.com>
2023-11-18 19:45:58 -0300

    fix: improves db init

--- init.sql
+++ init.sql
@@ -1,1 +1,7 @@
-CREATE DATABASE auth;
+CREATE DATABASE if not exists auth;
+
+insert into auth.user (name, email, password, role) values 
+('admin', 'admin@email.com', '$2b$10$vF6NSRJueu00q72mI6LgB.cBw/U8vwqQQgJQImUSoinhW0iYpYUHG', 1),
+('employer', 'employer@email.com', '$2b$10$vF6NSRJueu00q72mI6LgB.cBw/U8vwqQQgJQImUSoinhW0iYpYUHG', 2),
+('customer', 'customer@email.com', '$2b$10$vF6NSRJueu00q72mI6LgB.cBw/U8vwqQQgJQImUSoinhW0iYpYUHG', 3);
+
